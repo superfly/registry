beb9c0d804b4f18016976b0cc3320b6da8662a3f
Ryan Dahl <ry@tinyclouds.org>
2018-12-04 12:28:20 -0500

    Add index page.

--- x/test.js
+++ x/test.js
@@ -1,10 +1,17 @@
-
-const { proxy } = require("./app");
+const { proxy, indexPage } = require("./app");
 const assert = require("assert");
 
-assert.equal(proxy("/x/net/main.js"),
-  "https://raw.githubusercontent.com/denoland/net/master/main.js");
-assert.equal(proxy("/x/net/foo/bar.js"),
-  "https://raw.githubusercontent.com/denoland/net/master/foo/bar.js");
-assert.equal(proxy("/x/net@v0.1.2/foo/bar.js"),
-  "https://raw.githubusercontent.com/denoland/net/v0.1.2/foo/bar.js");
+assert.equal(
+  proxy("/x/net/main.js"),
+  "https://raw.githubusercontent.com/denoland/net/master/main.js"
+);
+assert.equal(
+  proxy("/x/net/foo/bar.js"),
+  "https://raw.githubusercontent.com/denoland/net/master/foo/bar.js"
+);
+assert.equal(
+  proxy("/x/net@v0.1.2/foo/bar.js"),
+  "https://raw.githubusercontent.com/denoland/net/v0.1.2/foo/bar.js"
+);
+
+console.log(indexPage());
