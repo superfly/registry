2eb66f4766b9e7e9ab24f6c2475a03eb53e79276
Ryan Dahl <ry@tinyclouds.org>
2019-02-01 22:40:37 -0500

    Add docs to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,5 @@ sam deploy --template-file packaged.yaml \
 #aws cloudfront create-invalidation --distribution-id E3NZSZMS5TZ0OU \
 #  --paths "/x/*"
 
-echo "Manually edit behaviors at https://console.aws.amazon.com/cloudfront/home?region=us-east-1#distribution-settings:E3NZSZMS5TZ0OU"
 echo "Manually update lambda https://console.aws.amazon.com/lambda/home?region=us-east-1"
+echo "then manually run:  aws cloudfront create-invalidation --distribution-id E3NZSZMS5TZ0OU --paths /x*"
