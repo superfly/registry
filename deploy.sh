f18b349692eddca2fa6170d10564c693c280b7e4
Ryan Dahl <ry@tinyclouds.org>
2019-02-19 19:51:09 -0500

    deploy kinda works

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,18 @@
 
 set -ev
 
+./test.sh
+
 aws cloudformation package \
   --template-file template.yaml \
   --s3-bucket deno.land  \
-  --s3-prefix cloudformation_package/  \
+  --s3-prefix cloudformation_package \
   --output-template-file packaged.yaml
 
 aws cloudformation deploy \
   --template-file packaged.yaml \
-  --stack-name denoland6
+  --stack-name denoland0 \
+  --capabilities CAPABILITY_IAM
+
+# aws cloudformation describe-stack-events --stack-name denoland0 | grep Err
+# aws cloudformation delete-stack --stack-name denoland0
